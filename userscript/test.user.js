ca01d8d1eff409d562cce20714720db343a16bd1
hugohu <511194948@qq.com>
2014-10-29 16:47:57 +0800

    test.user.js

---
+++ userscript/test.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         test
+// @namespace    http://huugle.org/
+// @version      0.1
+// @description  test
+// @author       huugle
+// @match        http://*.baidu.com/*
+// @grant        none
+// ==/UserScript==
+
+(function(){
+	console.log("test");
+})()
