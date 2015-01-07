a99890695d1852278471b52e7aa5cd4daea44078
hugohu <511194948@qq.com>
2015-01-07 17:01:51 +0800

    更新配置文件.

--- html/Makefile.js
+++ html/Makefile.js
@@ -1,24 +1,33 @@
 var fs = require('fs'),
-	fileList = '',
-	path = "./",
+	fileList = [],
+	path = ".",
 	filename = "config.js";
 //遍历目录生成目录名
 function walk(path) {
 	var dirList = fs.readdirSync(path);
 	dirList.forEach(function(item) {
 		if (fs.statSync(path + '/' + item).isDirectory()) {
-			fileList += '"' + item + '",';
+			walk(path + '/' + item);
+		} else if (item == "package.json") {
+			fileList.push(path + '/' + item);
 		}
 	});
 }
 walk(path);
 //生成配置文件.
-
-var data = "var items=[" + fileList.substring(0,fileList.length-1) + "];";
+function make(arr) {
+	var alist = [];
+	for (var i = 0; i < arr.length; i++) {
+		var data = fs.readFileSync(arr[i], 'utf-8');
+		alist.push(data);
+	};
+	return alist;
+}
+var data ='//make config by huugle '+ new Date() +'\nvar items=['+ make(fileList)+']';
 fs.writeFile(path + "/" + filename, data, 'utf8', function(err) {
 	if (err) {
 		console.log('err');
 	} else {
-		console.log('is ok');
+		console.log('the config is make ok.');
 	}
-})
+});
--- html/config.js
+++ html/config.js
@@ -1,1 +1,12 @@
-var items=["reg","userscript"];
+//make config by huugle Wed Jan 07 2015 17:01:03 GMT+0800 (中国标准时间)
+var items=[{
+	"name": "prinnko",
+	"version": "0.1.0",
+	"path":"reg",
+	"description":"一个正则工具"
+},{
+	"name": "prinnko",
+	"version": "0.1.0",
+	"path":"userscript",
+	"description":"userscript工具"
+}]
